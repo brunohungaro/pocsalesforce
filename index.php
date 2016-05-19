b0246ebd4aad3ef7bce9d885da6c73385ae90b5c
brunohungaro <brunohungaro@gmail.com>
2016-05-19 17:17:46 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -125,7 +125,7 @@ if (!$result) {
 							<div class="row">
 								<div class="col-xs-12 col-md-6">
 									<p class="lead">
-										<?php echo $row[11]; ?></p>
+										<?php echo $row[9]; ?></p>
 								</div>
 								<div class="col-xs-12 col-md-6">
 									<a class="btn btn-success" href="http://www.jquery2dotnet.com">Add to cart</a>
