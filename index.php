d0c8eb74d55946bffd7a2e9de6412b1e88669d7e
brunohungaro <brunohungaro@gmail.com>
2016-05-19 16:37:14 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+
+$con_string = "host=ec2-54-221-253-117.compute-1.amazonaws.com port=5432 dbname=db1ohqopoa0v9h user=wvvhtpxdeelsit password=nQAU36hPAOksIj3Clg9W3dJu2O";
+
+$bdcon4 = pg_connect($con_string);
+
+?>
+
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
