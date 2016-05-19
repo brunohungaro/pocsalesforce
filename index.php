ec69c3dbf82410f24a131cbcfaeffc6e1d3a8ea1
brunohungaro <brunohungaro@gmail.com>
2016-05-19 17:05:01 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ if (!$result) {
 }
 
 while ($row = pg_fetch_row($result)) {
-  echo "$row[0] - $row[1] - $row[2] - $row[3] - $row[4] - $row[5]";
+  echo "$row[0] - $row[1] - $row[2] - $row[3] - $row[4] - $row[5] - $row[6] - $row[7]";
   echo "<br />\n";
 }
 
