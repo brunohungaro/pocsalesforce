a55066881808c149a489e434a185f4db90895e7a
brunohungaro <brunohungaro@gmail.com>
2016-05-19 17:16:25 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $con_string = "host=ec2-54-221-253-117.compute-1.amazonaws.com port=5432 dbname=
 
 $bdcon = pg_connect($con_string);
 
-$result = pg_query($bdcon, "select name from pocsf.carro__c");
+$result = pg_query($bdcon, "select * from pocsf.carro__c");
 if (!$result) {
   echo "Erro na consulta.<br>";
   exit;
@@ -118,14 +118,14 @@ if (!$result) {
 						<img class="group list-group-image" src="http://placehold.it/400x250/000/fff" alt="" />
 						<div class="caption">
 							<h4 class="group inner list-group-item-heading">
-								<?php echo $row["name"]; ?></h4>
+								<?php echo $row[10]; ?></h4>
 							<p class="group inner list-group-item-text">
 								Product description... Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
 								sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
 							<div class="row">
 								<div class="col-xs-12 col-md-6">
 									<p class="lead">
-										$21.000</p>
+										<?php echo $row[11]; ?></p>
 								</div>
 								<div class="col-xs-12 col-md-6">
 									<a class="btn btn-success" href="http://www.jquery2dotnet.com">Add to cart</a>
